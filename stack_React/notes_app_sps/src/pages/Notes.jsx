1459e87c92b55ce6df351407347ed48a882c21d9
Dgaleano01 <degaleanocarmona@gmail.com>
2023-10-20 12:48:10 -0500

    notes

--- stack_React/notes_app_sps/src/pages/Notes.jsx
+++ stack_React/notes_app_sps/src/pages/Notes.jsx
@@ -1,24 +1,8 @@
 import Cards from "./Cards";
 import NavBar from "./NavBar";
-import { useEffect, useState } from "react";
-import { getNotesRequest } from "../api/notes"
 
 function Notes() {
 
-  const [notes, setNotes] = useState([]);
-
-  async function getNotes(){
-    try{
-      const data = await getNotesRequest();
-      setNotes(data.data);
-    }catch ( error ){
-      console.log(error)
-    }
-  }
-
-  useEffect(()=>{
-    getNotes()
-  }, []);
   return (
     <>
       <NavBar />
